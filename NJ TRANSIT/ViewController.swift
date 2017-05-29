cc8dd0643613144feef2636c16eb9b87edaee744
Matthew Bowyer <mattcboo.apple@gmail.com>
2017-05-29 13:54:34 -0400

    Initial Commit

---
+++ NJ TRANSIT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NJ TRANSIT
+//
+//  Created by Matt Bowyer on 5/29/17.
+//  Copyright © 2017 Grape. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
